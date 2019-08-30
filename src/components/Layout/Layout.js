161c62ca772db886db9c27098efb796d37e4ee74
Dinesh Kumar <dineshbaalakumar@gmail.com>
2019-08-30 16:40:58 +0530

    Sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,10 +4,12 @@ import Aux from "../../hoc/Auxiliary";
 import Classes from "./Layout.module.css";
 
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import Sidedrawer from '../Navigation/Sidedrawer/Sidedrawer';
 
 const layout = props => (
   <Aux>
     <Toolbar />
+    <Sidedrawer />
     <main className={Classes.Content}>{props.children}</main>
   </Aux>
 );
---
+++ src/components/Navigation/Sidedrawer/Sidedrawer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+
+import classes from "./Sidedrawer.module.css";
+
+const sideDrawer = () => {
+  return (
+    <div className={classes.Sidedrawer}>
+      <Logo height="11%" />
+      <nav>
+        <NavigationItems />
+      </nav>
+    </div>
+  );
+};
+
+export default sideDrawer;
