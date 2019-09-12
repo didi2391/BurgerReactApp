ff309c02653870cb033172e3e5932915e4fa106f
didi2391 <dineshbaalakumar@gmail.com>
2019-09-12 18:05:54 +0530

    Added redux dev tools extension

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createStore, applyMiddleware, combineReducers , compose} from "redux";
 import thunk from "redux-thunk";
 
 import "./styles.css";
@@ -11,12 +11,14 @@ import App from "../src/App";
 import burgerBuilderReducer from "./store/reducers/BurgerBuilder";
 import orderReducers from "./store/reducers/order";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const rootReducers = combineReducers({
   burgerBuilder: burgerBuilderReducer,
   order: orderReducers
 });
 
-const store = createStore(rootReducers, applyMiddleware(thunk));
+const store = createStore(rootReducers, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
   <Provider store={store}>
