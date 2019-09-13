558f05ad3be456ece95e9ef76ebd448eb4a02799
didi2391 <dineshbaalakumar@gmail.com>
2019-09-13 11:25:24 +0530

    Fixing total Price Issue

--- src/store/reducers/BurgerBuilder.js
+++ src/store/reducers/BurgerBuilder.js
@@ -42,6 +42,7 @@ const reducer = (state = initialState, action) => {
           cheese: action.ingredients.cheese,
           salad: action.ingredients.salad
         },
+        totalPrice : 4,
         error: false
       };
     case actionTypes.FETCH_INGREDIENTS_FAILED:
